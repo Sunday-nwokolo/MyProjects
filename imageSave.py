fdc16c7c3a7bee9cbd93bdf1051c86880e4abd62
Sunday Nwokolo <snn23kfl@bangor.ac.uk>
2023-10-23 12:35:28 +0100

    comit changes to 23October images

---
+++ imageSave.py
@@ -0,0 +1,17 @@
+from PIL import Image
+import os
+
+def load_images(directory):
+    image_files = [f for f in os.listdir(directory) if f.endswith(('png', 'jpg', 'jpeg'))]
+    return [Image.open(os.path.join(directory, f)) for f in image_files]
+
+def save_images(image_list):
+    for idx, img in enumerate(image_list, start=1):
+        # Format the index with zero padding using zfill
+        filename = f'23OctoberImage/whitefd/whitefd_{str(idx).zfill(6)}.jpg'
+        img.save(filename)
+        print(f'Image saved as {filename}')
+
+# Load images from a directory
+raw_images = load_images('C:/Users/snn23kfl/23OctoberImage/whitefd/')  #Replace with the path to your images
+save_images(raw_images)
